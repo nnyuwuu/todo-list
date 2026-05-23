890bf07b44f1184500f1e30fe8d1812ce09c43f6
nnyuwuu <nnyuwuu@gmail.com>
2026-05-23 18:06:07 +0600

    feat: add toggle complete, delete functionality and empty state with image

--- script.js
+++ script.js
@@ -43,11 +43,31 @@ function renderTodos(){
         deleteBtn.addEventListener("click", function(){
             deleteTodo(todo.id)
         })
+        const checkbox = li.querySelector('input[type="checkbox"]')
+        checkbox.addEventListener("change", function(){
+            toggleTodo(todo.id)
+        })
+        checkbox.checked = todo.completed
+        if (todo.completed) {
+            li.classList.add('todo__item--completed')
+}
         todoList.appendChild(li)
+        
     })
+    if (todos.length === 0) {
+        todoList.innerHTML = `<li class="todo__empty-img">
+            <img src="images/todo_luffy.png" alt="image for empty todo list" width="400">
+        </li>`
+    }
+    
 
 }
 function deleteTodo(id){
     todos = todos.filter(todo => todo.id !== id)
+    renderTodos()
+}
+function toggleTodo(id){
+    const todo = todos.find(todo => todo.id === id)
+    todo.completed = !todo.completed
     renderTodos()
 }
